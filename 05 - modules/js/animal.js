be304204a58ddf0a0b12c6c5ec341e578a33a48a
cleivas <clara.leivas@gmail.com>
2024-09-13 12:12:53 +0200

    added modules sample

---
+++ 05 - modules/js/animal.js
@@ -0,0 +1,15 @@
+const animal1 = {
+    species: 'Elephant',
+    name: 'Dumbo',
+    likes: ['Mud', 'Bathing', 'Flapping ears']
+}
+
+
+const animal2 = {
+    species: 'Squirrel',
+    name: 'Killer Squirrel',
+    likes: ['Biting', 'Nibbling', 'Teeth']
+}
+
+// console.log(animal1);
+export {animal1, animal2};
---
+++ 05 - modules/js/main.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+import {animal1, animal2} from './animal.js';
+import { displayRandom } from './display.js';
+
+displayRandom(animal1.name);
+displayRandom(animal2.name);
