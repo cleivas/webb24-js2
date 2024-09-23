ecb7ecb755fbeb0fbb88ba7d81c90d3f83182174
cleivas <clara.leivas@gmail.com>
2024-09-23 14:26:11 +0200

    removed firebaseConfig obj from firebaseapp.js

--- 07 - bibliotek/firebase/src/modules/firebaseapp.js
+++ 07 - bibliotek/firebase/src/modules/firebaseapp.js
@@ -4,13 +4,7 @@ import { getDatabase, ref} from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZnO_WNRJ1VTMG5gmHor1moRPGF2IsJFk",
-  authDomain: "firstdb-82c36.firebaseapp.com",
-  databaseURL: "https://firstdb-82c36-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "firstdb-82c36",
-  storageBucket: "firstdb-82c36.appspot.com",
-  messagingSenderId: "27321012590",
-  appId: "1:27321012590:web:c8eb5fc79d2f04a470717f"
+ // OBS kopiera dina uppgifter från firebase
 };
 
 // Initialize Firebase
